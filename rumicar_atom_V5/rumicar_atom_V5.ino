bf9d2e839d284f20e7ed6063c2e278a4c100c645
pokibon <ooe@k.email.ne.jp>
2020-05-31 23:48:05 +0900

    pre release

--- rumicar_atom_V5/rumicar_atom_V5.ino
+++ rumicar_atom_V5/rumicar_atom_V5.ino
@@ -26,7 +26,9 @@ char auth[] = "mcJxlsL7QpjgwQ6Rr-OGJVhbWw9jWiSp";
 int16_t Joy_X;
 int16_t Joy_Y;
 int16_t AutoPilot = 1;
-
+int16_t Trim;
+int16_t Max_Speed;
+WidgetLCD lcd(V3);
 //=========================================================
 //  Arduino setup function
 //=========================================================
@@ -35,15 +37,16 @@ void setup()
   RC_setup();
   Blynk.setDeviceName(DEVICE_NAME);
   Blynk.begin(auth);  
+  lcd.clear();
 }
 
+int s0, s1, s2;
 //=========================================================
 //  auto pilot function
 //=========================================================
 int iBuf = 0;
 void auto_pilot()
 {
-  int s0, s1, s2;
   int ispeed = 255;
   int idist1;
   int sDist = 150;
@@ -55,24 +58,6 @@ void auto_pilot()
   int sDrive;
   int dDist;  
 
-  //=========================================================
-  //  get VL53L0X TOF sensor value 
-  //    S0: left S1: center S2: right
-  //=========================================================
-  //s0=sensor0.readRangeContinuousMillimeters();
-  //s1=sensor1.readRangeContinuousMillimeters();
-  //s2=sensor2.readRangeContinuousMillimeters();
-  s0=sensor0.readRangeSingleMillimeters();
-  s1=sensor1.readRangeSingleMillimeters();
-  s2=sensor2.readRangeSingleMillimeters();
-/*
-  Serial.print("Sensor0:");
-  Serial.print(s0);
-  Serial.print("  Sensor1:");
-  Serial.print(s1);
-  Serial.print("  Sensor2:");
-  Serial.println(s2);
-*/
   //=========================================================
   //  drive  
   //=========================================================
@@ -109,6 +94,8 @@ void auto_pilot()
   if (s2 > 250) s2 = 250;
   else if (s2 < 50) s2 = 50;
   dAngle = (s0 - s2) / 2;
+  Serial.print("dAngle : ");
+  Serial.println(dAngle);
   if (dAngle > 10) RC_steer(LEFT, abs(dAngle));
   else if (dAngle < -10) RC_steer(RIGHT, abs(dAngle));  
   else RC_steer(CENTER);
@@ -120,20 +107,23 @@ void auto_pilot()
 void manual_pilot()
 {
   int sDrive;
+  int angle;
   int K_OFF = 50;    // original 64
-  if (Joy_Y > 0) {
-    sDrive = constrain( Joy_Y + K_OFF, 0, 255);
+  
+  if (Joy_Y > 10) {
+    sDrive = constrain( Joy_Y + K_OFF, 0, Max_Speed);
     RC_drive(FORWARD, sDrive);
-  } else if (Joy_Y < 0) {
-    sDrive = constrain(-Joy_Y + K_OFF, 0, 255);
+  } else if (Joy_Y < -10) {
+    sDrive = constrain(-Joy_Y + K_OFF, 0, Max_Speed);
     RC_drive(REVERSE, sDrive);    
   } else {
     RC_drive(BRAKE, 0); 
   }
-  if (Joy_X > 0) {
-    RC_steer(RIGHT, Joy_X);
-  } else if (Joy_X < 0) {
-    RC_steer(LEFT,-Joy_X);    
+  angle = constrain(Joy_X + Trim, -100, 100);
+  if (angle > 0) {
+    RC_steer(RIGHT, angle);
+  } else if (angle < 0) {
+    RC_steer(LEFT, -angle);    
   } else {
     RC_steer(CENTER); 
   }
@@ -146,6 +136,30 @@ void loop()
 {
   M5.update();
   Blynk.run();
+  char buf[32];
+    
+  //=========================================================
+  //  get VL53L0X TOF sensor value 
+  //    S0: left S1: center S2: right
+  //=========================================================
+  //s0=sensor0.readRangeContinuousMillimeters();
+  //s1=sensor1.readRangeContinuousMillimeters();
+  //s2=sensor2.readRangeContinuousMillimeters();
+  s0=sensor0.readRangeSingleMillimeters();
+  s1=sensor1.readRangeSingleMillimeters();
+  s2=sensor2.readRangeSingleMillimeters();
+
+  sprintf( buf, "%3dcm%3dcm%3dcm ", s0/10, s1/10, s2/10);
+  lcd.print(0,0, " LEFT CENT.RIGHT");
+  lcd.print(0, 1, buf);
+/*
+  Serial.print("Sensor0:");
+  Serial.print(s0);
+  Serial.print("  Sensor1:");
+  Serial.print(s1);
+  Serial.print("  Sensor2:");
+  Serial.println(s2);
+*/
   if (AutoPilot == 1) {
     auto_pilot();
   } else {
@@ -156,22 +170,32 @@ void loop()
 //=========================================================
 //  Bylnk Callback functions
 //=========================================================
+// V0: Auto Pilot Switch
 BLYNK_WRITE(V0) {
   AutoPilot = param[0].asInt();
-  Serial.println(AutoPilot);
-  if(AutoPilot == 1){
-    M5.dis.drawpix(0, 0x000000);
-  }
-  if(AutoPilot == 0){
-    M5.dis.drawpix(0, 0xf00000);
-  }
+//  Serial.print("AutoPilot = ");
+//  Serial.println(AutoPilot);  
 }
-
+// V1: Joy Stick
 BLYNK_WRITE(V1) {
   Joy_X = param[0].asInt();
   Joy_Y = param[1].asInt();
-  Serial.print("X = ");
-  Serial.print(Joy_X);
-  Serial.print("  Y = ");
-  Serial.println(Joy_Y);
+//  Serial.print("X = ");
+//  Serial.print(Joy_X);
+//  Serial.print("  Y = ");
+//  Serial.println(Joy_Y);
+}
+
+// V2: Trim
+BLYNK_WRITE(V2) {
+  Trim = param[0].asInt();
+//  Serial.print("Trim = ");
+//  Serial.println(Trim);
+}
+
+// V4: Max Speed
+BLYNK_WRITE(V4) {
+  Max_Speed = param[0].asInt();
+//  Serial.print("Max_Speed = ");
+//  Serial.println(Max_Speed);
 }
